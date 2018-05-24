dfdf47badeeb122ae8c255381a61b112ffedd03f
linpeijie <lpj498840744@qq.com>
2018-05-24 20:51:27 +0800

    测试模型准确率，使用鸢尾花数据集

--- KNN/test.py
+++ KNN/test.py
@@ -1,32 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from kNN import KNNClassifier
+from model_selection import train_test_split
 import pandas as pd
 import numpy as np
-
-
-def data_set():
-    """
-    :鸢尾花数据集,返回处理后的数据
-    :rtype: ndarray
-    """
-    file = '../iris.data'
-    df = pd.read_csv(file, header=None)
-    # 处理数据集，转换成array格式
-    y = df.loc[0:, 4].values
-    x = df.loc[0:, [0, 2]].values
-
-    return x, y
 
 
 if __name__ == '__main__':
     """
     """
+    iris = pd.read_csv('../iris.data', header=None)
+    iris_data = iris.loc[:, :].values
+    x_data = iris_data[:, [0, 2]]
+    y_data = iris_data[:, 4]
+
     x_predict = np.array([[5.1, 2.1]])
-    x_train, y_train = data_set()
+    x_train, y_train, x_test, y_test = train_test_split(x_data, y_data)
 
-    knn_clf = KNNClassifier(k=6)
+    knn_clf = KNNClassifier(k=3)
     knn_clf.fit(x_train, y_train)
-    y_predict = knn_clf.predict(x_predict)
+    y_predict = knn_clf.predict(x_test)
 
-    print('预测结果为：{}'.format(y_predict))
+    print('预测准确率：{:.2f}%'.format((sum(y_predict == y_test)/len(y_test)) * 100))
