c41d84b18bb5438d2a166a25e8d120469b7c3ed4
linpeijie <lpj498840744@qq.com>
2018-05-29 16:42:06 +0800

    add

---
+++ decision_tree/decision_tree.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def entropy(p):
+    """计算信息熵
+    :param p:
+    :return:
+    """
+    return
+
+def plot_decision_boundary(model, axis):
+    """
+    :param model:
+    :param axis:
+    :return:
+    """
+
+
+if __name__=='__main__':
