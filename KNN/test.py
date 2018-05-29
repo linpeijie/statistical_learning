b237c51bac25e688b2e36778f9b542d038a4bf0b
linpeijie <lpj498840744@qq.com>
2018-05-29 16:41:21 +0800

    test

--- KNN/test.py
+++ KNN/test.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from kNN import KNNClassifier
+from KNN import KNNClassifier
 from model_selection import train_test_split
+from metric import accuracy_score
 import pandas as pd
 import numpy as np
 
@@ -17,8 +18,15 @@ if __name__ == '__main__':
     x_predict = np.array([[5.1, 2.1]])
     x_train, y_train, x_test, y_test = train_test_split(x_data, y_data)
 
-    knn_clf = KNNClassifier(k=3)
-    knn_clf.fit(x_train, y_train)
-    y_predict = knn_clf.predict(x_test)
+    best_score = 0.0
+    best_k = -1
+    for k in range(1, 11):
+        knn_clf = KNNClassifier(n_neighbors=k)
+        knn_clf.fit(x_train, y_train)
+        score = knn_clf.score(x_test, y_test)
+        if score > best_score:
+            best_k = k
+            best_score = score
 
-    print('预测准确率：{:.2f}%'.format((sum(y_predict == y_test)/len(y_test)) * 100))
+    print("best_k =", best_k)
+    print('best_score =', best_score)
