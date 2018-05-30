22689f28c19e741406c4d7e8e17487bb8bf8edc1
linpeijie <lpj498840744@qq.com>
2018-05-30 16:56:31 +0800

    计算信息增益

---
+++ decision_tree/test.py
@@ -0,0 +1,22 @@
+
+from decision_tree import DecisionTreeID3
+import pandas as pd
+
+
+if __name__ == '__main__':
+    """
+    table = pd.read_csv('../table_5_1.data',header=None)
+    table_data = table.loc[:, :].values
+    x = table_data[1:, :5]
+    y = table_data[1:, 5]
+    x_train, y_train, x_test, y_test = train_test_split(x, y)
+    print(x_train)
+    """
+    table = pd.read_csv('../table_5_1.data', header=None)
+    table_data = table.loc[:, :].values
+    x = table_data[1:, :4]
+    y = table_data[1:, 4]
+
+    my_dt = DecisionTreeID3()
+    my_dt.entropy(x, y)
+    
