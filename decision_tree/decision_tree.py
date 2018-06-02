be1cf46ce7c5455df48db70004bd54493319988b
linpeijie <lpj498840744@qq.com>
2018-06-02 18:38:39 +0800

    add

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,15 +1,57 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
+from collections import Counter
 import pandas as pd
 
 
 class DecisionTreeID3:
     """
     """
-    def __init__(self):
-        """"""
+    def __init__(self, x, y):
+        """初始化
+        : e 为阈值
+        """
+        self.x_train = x
+        self.y_train = y
+        self.e = 0.001
+
+    def create_tree(self, x, y):
+        """构建决策树
+        :param x:
+        :param y:
+        :return:
+        """
+        # （1）若D中所有实例属于同一类C，将此类C作为节点的类标记，返回T，即创建 叶子节点
+        if self.empirical_entropy(y) == 0:
+            print(y)
+            return y[0]
+        # （2）若特征A为空集，则将剩下的所有无法再继续分类的样本点划分到含有最多样本点的类C中，创建 叶子节点
+        if len(x[0]) == 1:
+            return self.max_label()
+        # (3)应用算法5.1 计算信息增益，选择其中信息增益最大的特征A
+        feature_entropy = self.entropy(x, y)
+        best_feature = np.argwhere(np.max(feature_entropy) == feature_entropy)[0][0]
+        # (4)如果A的信息增益小于阈值e，将该节点设为叶子结点，且将含有最多样本点的类C作为其值，
+        if best_feature < self.e:
+            return self.max_label()
+        # 初始化树T
+        tree = {best_feature: {}}
+        # (5)分割特征空间，(6)递归的调用树，直到所有情况都确定为止
+        feature = x[:, best_feature]
+        for value in np.unique(feature):
+            x, y = self.split_feature(x, y, best_feature, value)
+            tree[best_feature][value] = self.create_tree(x, y)
+        return tree
+
+    def max_label(self):
+        """找出数据集X中实例数最大的类C
+        :return: label
+        """
+        return Counter(self.y_train).most_common(1)[0][0]
 
     def entropy(self, x_train, y_train):
-        """计算信息熵及信息增益
+        """计算信息熵及信息增益,返回全部特征的信息增益 数组
         :param x_train: array
         :param y_train: array
         :return: List
@@ -72,13 +114,57 @@ class DecisionTreeID3:
         """
         assert label_y is not None, 'the label_y must be valid'
 
-        emp_entropy = 0
-        # 样本容量
-        data_size = label_y.size
-        # 计算类的可能取值元素
-        label = np.unique(label_y)
-        # 计算经验熵
-        for k in range(len(label)):
-            e = float(np.sum(label[k] == label_y)) / float(data_size)
-            emp_entropy -= e * np.log2(e)
+        # 统计类别数量
+        counter = Counter(label_y)
+        emp_entropy = 0.0
+        for num in counter.values():
+            p = num / len(label_y)
+            emp_entropy += -p * np.log2(p)
         return emp_entropy
+
+    def split_feature(self, x, y, index, value):
+        """划分特征空间 A，返回划分后的子集
+        :param x:
+        :param y:
+        :param index:
+        :param value:
+        :return:
+        """
+        assert  x is not None and y is not None, 'x is None, y is None'
+        # 取出符合条件的样本点
+        feature = x[x[:, index] == value,:]
+        y = y[x[:, index] == value]
+        return feature, y
+
+    @staticmethod
+    def split(x, y, d, value):
+        """对特征空间进行划分
+        :param x:
+        :param y:
+        :param d:
+        :param value:
+        :return:
+        """
+        index_a = (x[:, d] <= value)
+        index_b = (x[:, d] > value)
+        return x[index_a], x[index_b], y[index_a], y[index_b]
+
+    def try_split(self, x, y):
+        """寻找最优划分,使得整棵树信息熵降低，这个是计算连续性数值，并且是自动选择需要划分的特征A，而不是已经选择好了
+        :param x:
+        :param y:
+        :return:
+        """
+        best_entropy = float('inf')
+        best_d, best_v = -1, -1
+        # 对x每个维度进行遍历,即遍历 列
+        for d in range(x.shape[1]):
+            sorted_index = np.argsort(x[:, d])
+            for i in range(1, len(x)):  # 对每个样本进行遍历，找相邻的两个点中间的d是多少
+                if x[sorted_index[i-1], d] != x[sorted_index[i], d]:
+                    v = (x[sorted_index[i-1], d] + x[sorted_index[i], d]) / 2
+                    x_l, x_r, y_l, y_r = self.split(x, y, d, v)
+                    e = self.empirical_entropy(y_l) + self.empirical_entropy(y_r)
+                    if e < best_entropy:
+                        best_entropy, best_d, best_v = e, d, v
+        return best_entropy, best_d, best_v
--- decision_tree/test.py
+++ decision_tree/test.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 
 from decision_tree import DecisionTreeID3
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
@@ -14,10 +17,8 @@ if __name__ == '__main__':
     """
     table = pd.read_csv('../table_5_1.data', header=None)
     table_data = table.loc[:, :].values
-    x = table_data[1:, :4]
-    y = table_data[1:, 4]
+    x = table_data[:, :4]
+    y = table_data[:, 4]
 
-    my_dt = DecisionTreeID3()
-    my_dt.entropy(x, y)
-    my_dt.entropy_ratio(x, y)
-    
+    my_dt = DecisionTreeID3(x, y)
+    my_dt.create_tree(x, y)
